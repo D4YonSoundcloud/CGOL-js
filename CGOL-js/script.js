be4cad96fbece03cd3f310474a0f68a2277f9493
d4yonsouncloude <mattd4y@gmail.com>
2020-05-19 15:46:25 -0600

    backboneCreated+MuchMoreNeeded5:46pm5-19-20

--- CGOL-js/script.js
+++ CGOL-js/script.js
@@ -1,3 +1,6 @@
+//to do!
+//make each cell an object
+
 function make2DArray(cols, rows) {
   let arr = new Array(cols);
   for (let i = 0; i < arr.length; i++) {
@@ -6,11 +9,20 @@ function make2DArray(cols, rows) {
   return arr;
 }
 
+// class Cell {
+//     constructor(x, y) {
+//         this.x = x;
+//         this.y = y;
+//         this.state = this.state;
+//     }
+
+// }
+
 let grid;
 let cols;
 let rows;
 //resolution decides the size of each cell
-let resolution = 20;
+let resolution = 10;
 
 function setup() {
   createCanvas(600, 400);
@@ -19,8 +31,11 @@ function setup() {
   rows = height / resolution;
 
   grid = make2DArray(cols, rows);
+  // i = col or x
+  // j = row or y
   for (let i = 0; i < cols; i++) {
     for (let j = 0; j < rows; j++) {
+      //new Cell(i,j)
       grid[i][j] = floor(random(2));
     }
   }
@@ -28,6 +43,7 @@ function setup() {
 
 function draw() {
   background(0);
+
   for (let i = 0; i < cols; i++) {
     for (let j = 0; j < rows; j++) {
       //x and y determine position of each cell, which is find by using the col or row index * the resolution
@@ -39,8 +55,53 @@ function draw() {
       if (grid[i][j] == 1) {
         fill(255);
         stroke(0);
-        rect(x, y, resolution-1, resolution-1);
+        rect(x, y, resolution - 1, resolution - 1);
       }
     }
   }
+  let next = make2DArray(cols, rows);
+  //compute next generation based on the past grid
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      let state = grid[i][j];
+
+      //Count alive neighbors
+      let sum = 0;
+      let neighbors = countNeighbors(grid, i, j);
+
+      if (state == 0 && neighbors == 3) {
+        next[i][j] = 1;
+      } else if (state == 1 && (neighbors < 2 || neighbors > 3)) {
+        next[i][j] = 0;
+      } else {
+        next[i][j] = state;
+      }
+    }
+  }
+
+  grid = next;
 }
+
+function countNeighbors(grid, x, y) {
+  let sum = 0;
+  for (let i = -1; i < 2; i++) {
+    for (let j = -1; j < 2; j++) {
+      let col = (x + i + cols) % cols;
+      let row = (y + j + rows) % rows;
+
+      sum += grid[col][row];
+    }
+  }
+
+  sum -= grid[x][y];
+  return sum;
+}
+
+//   sum += grid[i - 1][j - 1];
+//   sum += grid[i][j - 1];
+//   sum += grid[i + 1][j - 1];
+//   sum += grid[i + 1][j];
+//   sum += grid[i + 1][j + 1];
+//   sum += grid[i][j + 1];
+//   sum += grid[i - 1][j];
+//   sum += grid[i - 1][j + 1];
